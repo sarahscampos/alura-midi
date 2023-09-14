363f07a7c9ea9558ffe0f5d855d818cc15ec76d9
sarah-campos <sarahstephanycampos@gmail.com>
2023-09-14 14:38:31 -0300

    otimizacao

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
+//referencia constante
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+//referencia variavel
+//let contador = 0;
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    console.log(contador)
+}
