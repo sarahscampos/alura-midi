243707d35588dc4ae332ecdf4295cdbbefdc7413
sarah-campos <sarahstephanycampos@gmail.com>
2023-09-16 00:18:27 -0300

    funcionalidade teclado

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento != null && elemento.localName == 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert("Elemento não encontrado");
+        console.log("Elemento não encontrado ou seletor inválido");    
+    }
+   
 }
 
 //referencia constante
@@ -12,17 +21,29 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 for(let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-
     const instrumento = tecla.classList[1];
-
     //template string
     const idAudio = `#som_${instrumento}`;
 
-    console.log(idAudio);
+    //console.log(idAudio);
 
     tecla.onclick = function() {
         tocaSom(idAudio);
     }
 
-    console.log(contador)
+    tecla.onkeydown = function(evento) {
+
+        console.log(evento.code);
+
+        //Em JS == compara o valor, já === compara o tipo e o valor dos dados
+        if(evento.code == 'Space' || evento.code == 'Enter') {
+            tecla.classList.add('ativa'); //adiciona classe 
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa'); //remove classe
+    }
+
+    //console.log(contador)
 }
